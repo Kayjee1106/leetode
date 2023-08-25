35319312200c32f9f75849ee2882724773bb8dd5
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2023-08-26 03:09:20 +0530

    Time: 7 ms (5.75%), Space: 41.4 MB (5.04%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        String [] arr1=jewels.split("");
+        String [] arr2=stones.split("");
+        HashMap<String,Boolean> hm=new HashMap<>();
+        for(String i:arr1){
+            hm.put(i,true);
+        }
+        for(String j:arr2){
+            if(hm.get(j)!=null){
+                count++;
+            }
+        }
+        return count;
+    }
+}
