3da099e718821a3785fe9a59173a339f3dd2e2de
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-05-20 15:12:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (82.48%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,13 +1,10 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        return solve(nums,0,0);
-    }
-   int solve(int[] nums,int i,int Xor){
-        if(i==nums.length){
-            return Xor;
+        int n =nums.length;
+        int result=0;
+        for(int i:nums){
+            result|=i;
         }
-        int include=solve(nums,i+1,nums[i]^Xor);
-        int exclude=solve(nums,i+1,Xor);
-        return include+exclude;
+        return result<<(n-1);
     }
 }
