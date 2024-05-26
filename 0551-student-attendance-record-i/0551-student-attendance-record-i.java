41985aceeabc7645ece4246d9ba25e76674d0d8e
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-05-26 14:36:47 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (99.69%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.java
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -5,11 +5,14 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='A'){
                 a++;
+                if(a>=2){
+                    return false;
+                }
             }
             else if(i<=s.length()-3 && s.charAt(i+1)=='L'  && s.charAt(i+2)=='L' && s.charAt(i)=='L'
 ){               l=false;
             }
         }
-        return (a<2 && l);
+        return (l);
     }
 }
