660ecb2ba1a4133732d3ff2bf0f59e1d31e25cf7
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-05-27 16:39:33 +0530

    Time: 2 ms (47.70%), Space: 41.1 MB (68.83%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int lowerBound(int[] nums,int x){
+        int left = 0;
+        int right = nums.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= x) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        for(int x=0;x<=n;x++){
+            int i=lowerBound(nums,x);
+            if(n-i==x){
+                return x;
+            }
+        }
+        return -1;
+    }
+}
