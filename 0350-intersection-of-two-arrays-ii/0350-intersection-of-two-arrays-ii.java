13d0f70054c54d0ad21bff3b1416c5b87525b45a
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-02 15:36:24 +0530

    Time: 3 ms (90.95%), Space: 43.3 MB (29.62%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,14 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int n1=nums1.length;
-        int n2=nums2.length;
-        Integer[] nums1Integer = Arrays.stream(nums1).boxed().toArray(Integer[]::new);
-        Integer[] nums2Integer = Arrays.stream(nums2).boxed().toArray(Integer[]::new);
-        ArrayList<Integer> num1 = new ArrayList<>(Arrays.asList(nums1Integer));
-        ArrayList<Integer> num2 = new ArrayList<>(Arrays.asList(nums2Integer));
-        ArrayList<Integer> result=new ArrayList<>();
-        if (n1 < n2) {
-            for (Integer i : num1) {
-                if (num2.contains(i)) {
-                    result.add(i);
-                    num2.remove(i);
-                }
-            }
-        } else {
-            for (Integer i : num2) {
-                if (num1.contains(i)) {
-                    result.add(i);
-                    num1.remove(i);
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.getOrDefault(num, 0) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1); 
             }
         }
 
