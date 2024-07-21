123d88d15f460db12a42e1378091f8c0c5dbf4c9
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-22 00:28:48 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (13.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,6 +3,12 @@ class Solution {
         if(n<=1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        int arr[]=new int[n+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
     }
 }
