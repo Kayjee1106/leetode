56ae5ca1390c3fdd1e608d69074b894a1473ab2e
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-23 16:43:11 +0530

    Time: 191 ms (26.04%), Space: 41.7 MB (27.63%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,38 @@
+class Solution {
+    int m,n,l;
+    int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+    public boolean exist(char[][] board, String word) {
+         m=board.length;
+         n=board[0].length;
+         l=word.length();
+        if(m*n<1){
+            return false;
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j] == word.charAt(0) && find(board, i, j, word, 0)) {
+                return true;
+            }
+            }
+        }
+        return false;
+    }
+    public boolean find(char[][]board,int i,int j,String word,int idx){
+        if(idx >= l)
+        return true;
+        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(idx))
+        return false;
+        char temp = board[i][j];
+        board[i][j] = '$';
+        for(int[] dir : directions) {
+        int x = i + dir[0];
+        int y = j + dir[1];
+
+        if(find(board, x, y, word, idx+1))
+            return true;
+    }
+
+    board[i][j] = temp;
+    return false;
+    }
+}
