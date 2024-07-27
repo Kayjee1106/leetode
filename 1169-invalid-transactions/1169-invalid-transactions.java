5bef6b1fb5aa2fda44bec3dcc767f9a37c62e9f0
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-27 15:03:54 +0530

    Time: 9 ms (86.10%), Space: 45.5 MB (45.62%) - LeetHub

---
+++ 1169-invalid-transactions/1169-invalid-transactions.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    class Transaction {
+        String name;
+        int time;
+        int amount;
+        String city;
+
+        public Transaction(String s) {
+            String[] split = s.split(",");
+            name = split[0];
+            time = Integer.parseInt(split[1]);
+            amount = Integer.parseInt(split[2]);
+            city = split[3];
+        }
+    }
+
+    public List<String> invalidTransactions(String[] transactions) {
+        int n = transactions.length;
+        List<String> ans = new ArrayList<>();
+        HashMap<String, List<Transaction>> map = new HashMap<>();
+        
+        for (String trans : transactions) {
+            Transaction t = new Transaction(trans);
+            map.putIfAbsent(t.name, new ArrayList<>());
+            map.get(t.name).add(t);
+        }
+
+        for (String transaction : transactions) {
+            Transaction t = new Transaction(transaction);
+            if (!isValid(t, map.getOrDefault(t.name, new ArrayList<>()))) {
+                ans.add(transaction);
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean isValid(Transaction s, List<Transaction> list) {
+        if (s.amount > 1000) return false;
+        for (Transaction sol : list) {
+            if (Math.abs(sol.time - s.time) <= 60 && !s.city.equals(sol.city)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
