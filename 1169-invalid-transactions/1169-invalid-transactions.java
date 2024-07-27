bbcd5dda48b1a743aecc431c8a00e99b31214aff
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-27 15:08:34 +0530

    Time: 15 ms (48.34%), Space: 45.4 MB (45.62%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.java
+++ 1169-invalid-transactions/1169-invalid-transactions.java
@@ -1,49 +1,41 @@
-import java.util.*;
-
 class Solution {
-    class Transaction {
+    class Transaction{
         String name;
         int time;
         int amount;
         String city;
-
-        public Transaction(String s) {
-            String[] split = s.split(",");
-            name = split[0];
-            time = Integer.parseInt(split[1]);
-            amount = Integer.parseInt(split[2]);
-            city = split[3];
-        }
+        public Transaction(String s){
+        String[] split=s.split(",");
+        name=split[0];
+        time=Integer.parseInt(split[1]);
+        amount=Integer.parseInt(split[2]);
+        city=split[3];
     }
-
-    public List<String> invalidTransactions(String[] transactions) {
-        int n = transactions.length;
-        List<String> ans = new ArrayList<>();
-        HashMap<String, List<Transaction>> map = new HashMap<>();
-        
-        for (String trans : transactions) {
-            Transaction t = new Transaction(trans);
-            map.putIfAbsent(t.name, new ArrayList<>());
-            map.get(t.name).add(t);
+}
+        public List<String> invalidTransactions(String[] transactions) {
+        int n=transactions.length;
+        List<String> ans=new ArrayList<>();
+        HashMap<String,List<Transaction>> map=new HashMap<>();
+        for(String trans : transactions){
+            Transaction s=new Transaction(trans);
+            map.putIfAbsent(s.name,new ArrayList<>());
+            map.get(s.name).add(s);
         }
-
-        for (String transaction : transactions) {
-            Transaction t = new Transaction(transaction);
-            if (!isValid(t, map.getOrDefault(t.name, new ArrayList<>()))) {
+        for(String transaction : transactions){
+            Transaction t=new Transaction(transaction);
+            if(!isValid(t,map.getOrDefault(t.name,new ArrayList<>()))){
                 ans.add(transaction);
             }
         }
-
         return ans;
     }
-
-    public boolean isValid(Transaction s, List<Transaction> list) {
-        if (s.amount > 1000) return false;
-        for (Transaction sol : list) {
-            if (Math.abs(sol.time - s.time) <= 60 && !s.city.equals(sol.city)) {
+    public boolean isValid(Transaction s,List<Transaction> list){
+        if(s.amount>1000)return false;
+        for(Transaction sol:list){
+            if(Math.abs(sol.time-s.time)<=60 && !s.city.equals(sol.city)){
                 return false;
             }
         }
         return true;
-    }
+    } 
 }
