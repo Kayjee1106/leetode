5b3ec763e38d2b6c18bef3ff6a21bd81882145ea
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-30 03:06:02 +0530

    Time: 1 ms (74.12%), Space: 40.8 MB (98.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,20 +1,29 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
+//         if (strs == null || strs.length == 0) {
+//             return "";
+//         }
 
-        String prefix = strs[0];
-
-        for (int i = 1; i < strs.length; i++) {
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "";
-                }
+//         String prefix = strs[0];
+//         for (int i = 1; i < strs.length; i++) {
+//             while (strs[i].indexOf(prefix) != 0) {
+//                 prefix = prefix.substring(0, prefix.length() - 1);
+//                 if (prefix.isEmpty()) {
+//                     return "";
+//                 }
+//             }
+//         }
+        StringBuffer prefix=new StringBuffer();
+        Arrays.sort(strs);
+        for(int i=0;i<strs[0].length();i++){
+            if(i<strs[strs.length-1].length() && strs[0].charAt(i)==strs[strs.length-1].charAt(i)){
+                prefix.append(strs[0].charAt(i));
+            }
+            else{
+                break;
             }
         }
 
-        return prefix;
+        return prefix.toString();
     }
 }
