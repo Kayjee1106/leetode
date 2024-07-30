d76dd9b8016984e3db0b2b32db2be847dd2b6929
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-31 02:58:53 +0530

    Time: 9 ms (96.46%), Space: 43.5 MB (67.24%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,38 +1,79 @@
+// class Solution {
+//     int dp[][];
+//     private boolean solve(String s, int i,int j){
+//         if(i>=j){
+//             return true;
+//         }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j]==1;
+//         }
+//         if(s.charAt(i)==s.charAt(j)){
+//             dp[i][j]= solve(s,i+1,j-1)? 1:0;
+//         }
+//         else{
+//           dp[i][j]=0;  
+//         }
+//         return dp[i][j]==1;
+//     }
+//     public String longestPalindrome(String s) {
+//         int n=s.length();
+//         int max=0;
+//         int sp=0;
+//         dp=new int[n][n];
+//         for(int i=0;i<n;i++){
+//             Arrays.fill(dp[i],-1);
+//         }
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(solve(s,i,j)){
+//                     if(j-i+1>max){
+//                         max=j-i+1;
+//                         sp=i;
+//                     }
+//                 }
+//             }
+//         }
+//         return s.substring(sp,sp+max);
+//     }
+// }
 class Solution {
-    int dp[][];
-    private boolean solve(String s, int i,int j){
-        if(i>=j){
-            return true;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j]==1;
-        }
-        if(s.charAt(i)==s.charAt(j)){
-            dp[i][j]= solve(s,i+1,j-1)? 1:0;
-        }
-        else{
-          dp[i][j]=0;  
-        }
-        return dp[i][j]==1;
-    }
     public String longestPalindrome(String s) {
-        int n=s.length();
-        int max=0;
-        int sp=0;
-        dp=new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        StringBuilder t = new StringBuilder("$#");
+        for(char ch : s.toCharArray()) {
+            t.append(ch).append('#');
         }
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s,i,j)){
-                    if(j-i+1>max){
-                        max=j-i+1;
-                        sp=i;
-                    }
-                }
+        t.append('@');
+
+        int n = t.length();
+        int[] radii = new int[n];
+        int center = 0, radius = 0;
+        for(int i = 0; i < n; i++) {
+            int mirror = 2 * center - i;
+            if(i < radius) {
+                radii[i] = Math.min(radius - i, radii[mirror]);
+            }
+            while(i + 1 + radii[i] < n && 
+                  i - 1 - radii[i] >= 0 &&
+                  t.charAt(i + (1 + radii[i])) == t.charAt(i - (1 + radii[i]))) {
+                radii[i]++;
+            }
+            if(i + radii[i] > radius) {
+                center = i;
+                radius = i + radii[i];
+            }
+
+        }
+
+        int maxl = 0;
+        int centeri = 0;
+        for(int i = 0; i < n; i++) {
+            if(radii[i] > maxl) {
+                maxl = radii[i];
+                centeri = i;
             }
         }
-        return s.substring(sp,sp+max);
+        int start = (centeri - maxl) / 2;
+        String ans = s.substring(start, start + maxl);
+        return ans;
     }
 }
