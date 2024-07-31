85f9b60ea4a1bc804568ae542a87cce9f6bfd2f6
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-07-31 16:33:14 +0530

    Time: 8 ms (37.63%), Space: 47.8 MB (49.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,44 @@
 class Solution {
-    private String solve(String s){
-        int [] count=new int[26];
-         for(char ch : s.toCharArray()){
-            count[ch-'a']++;
-        }
-        StringBuilder temp=new StringBuilder();
-        for(int i=0;i<26;i++){
-            if(count[i]>0){
-                temp.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
+    // private String solve(String s){
+    //     int [] count=new int[26];
+    //      for(char ch : s.toCharArray()){
+    //         count[ch-'a']++;
+    //     }
+    //     StringBuilder temp=new StringBuilder();
+    //     for(int i=0;i<26;i++){
+    //         if(count[i]>0){
+    //             temp.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
+    //         }
+    //     }
+    //     return temp.toString();
+    // }
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     HashMap<String,List<String>> map=new HashMap<>();
+    //     for(String s:strs){
+    //         String temp=solve(s);
+    //         if(!map.containsKey(temp)){
+    //             map.put(temp,new ArrayList<>());
+    //         }
+    //         map.get(temp).add(s);
+    //     }
+    //     return new ArrayList<>(map.values());
+    // }
+        public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
+            
+            map.get(sortedWord).add(word);
         }
-
-        return temp.toString();
-    }
-    public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map=new HashMap<>();
-        for(String s:strs){
-            String temp=solve(s);
-            if(!map.containsKey(temp)){
-                map.put(temp,new ArrayList<>());
-            }
-            map.get(temp).add(s);
-        }
+        
         return new ArrayList<>(map.values());
     }
+    
 }
