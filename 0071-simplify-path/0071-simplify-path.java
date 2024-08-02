bf55772836712a5c7e18db33ee5b7c0e96ae62c5
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-03 02:35:28 +0530

    Time: 4 ms (87.38%), Space: 43 MB (90.13%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,22 +1,47 @@
+// class Solution {
+//     public String simplifyPath(String path) {
+//         String[] str=path.split("/");
+//         Stack<String> st=new Stack<>();
+//         String result="";
+//         for(int i=0;i<str.length;i++){
+//             if(str[i].equals(".")|| str[i].equals(""))continue;
+//             if(str[i].equals("..")){
+//                 if(!st.isEmpty())st.pop();
+//             }
+//             else st.push(str[i]);
+//         }
+//         while(!st.isEmpty()){
+//             result="/"+st.peek()+result;
+//             st.pop();
+//         }
+//          if (result.isEmpty()){
+//             result = "/";
+//         }
+//         return result;
+//     }
+// }
 class Solution {
     public String simplifyPath(String path) {
-        String[] str=path.split("/");
-        Stack<String> st=new Stack<>();
-        String result="";
-        for(int i=0;i<str.length;i++){
-            if(str[i].equals(".")|| str[i].equals(""))continue;
-            if(str[i].equals("..")){
-                if(!st.isEmpty())st.pop();
+        String[] parts = path.split("/");
+        Deque<String> deque = new LinkedList<>();
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!deque.isEmpty()) {
+                    deque.pollLast();
+                }
+            } else {
+                deque.offerLast(part);
             }
-            else st.push(str[i]);
         }
-        while(!st.isEmpty()){
-            result="/"+st.peek()+result;
-            st.pop();
+
+        StringBuilder result = new StringBuilder();
+        for (String dir : deque) {
+            result.append("/").append(dir);
         }
-         if (result.isEmpty()) {
-            result = "/";
-        }
-        return result;
+
+        return result.length() > 0 ? result.toString() : "/";
     }
 }
