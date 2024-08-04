50385decd30d29f397ecff1847ba6a500ca7e15b
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-05 01:29:18 +0530

    Time: 2 ms (36.43%), Space: 42.5 MB (59.14%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,35 +1,33 @@
 class Solution {
     public boolean isNumber(String s) {
-        if (s == null || s.trim().isEmpty()) return false;
-        
-        s = s.trim();
-        boolean decimalSeen = false;
-        boolean numberSeen = false;
-        boolean eSeen = false;
-        boolean numberAfterE = true; // To handle edge case when 'e' is at the end
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            switch (ch) {
+        if(s==null || s.trim().length()==0) return false;
+        boolean decimalSeen=false;
+        boolean numberSeen=false;
+        boolean eSeen=false;
+        boolean numberAfterE = true;
+        s.trim();
+        for(int i=0;i<s.length();i++){
+            Character ch=s.charAt(i);
+            switch(ch){
                 case '.':
-                    if (decimalSeen || eSeen) return false;
-                    decimalSeen = true;
+                    if(decimalSeen || eSeen)return false;
+                    decimalSeen=true;
                     break;
-                case 'e':
                 case 'E':
-                    if (eSeen || !numberSeen) return false;
-                    eSeen = true;
-                    numberAfterE = false; // Need to have a number after 'e'
+                case 'e':
+                    if(eSeen || !numberSeen) return false;
+                    eSeen=true;
+                    numberAfterE=false;
                     break;
                 case '+':
                 case '-':
-                    if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
-                    numberSeen = false; // Reset numberSeen for cases like '2e+2'
+                    if(i!=0 && s.charAt(i-1)!='e' && s.charAt(i - 1) != 'E')return false;
+                    numberSeen=false;
                     break;
                 default:
-                    if (!Character.isDigit(ch)) return false;
-                    numberSeen = true;
-                    numberAfterE = true;
+                    if(!Character.isDigit(ch))return false;
+                    numberSeen=true;
+                    numberAfterE=true;
             }
         }
         return numberSeen && numberAfterE;
