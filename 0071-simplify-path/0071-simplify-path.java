b468c5dc3f37d4eed278a2d4e49afe000c3bf3b7
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-06 00:02:47 +0530

    Time: 7 ms (29.30%), Space: 44.7 MB (10.29%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,45 +1,45 @@
-// class Solution {
-//     public String simplifyPath(String path) {
-//         String[] str=path.split("/");
-//         Stack<String> st=new Stack<>();
-//         String result="";
-//         for(int i=0;i<str.length;i++){
-//             if(str[i].equals(".")|| str[i].equals(""))continue;
-//             if(str[i].equals("..")){
-//                 if(!st.isEmpty())st.pop();
-//             }
-//             else st.push(str[i]);
-//         }
-//         while(!st.isEmpty()){
-//             result="/"+st.peek()+result;
-//             st.pop();
-//         }
-//          if (result.isEmpty()){
-//             result = "/";
-//         }
-//         return result;
-//     }
-// }
 class Solution {
     public String simplifyPath(String path) {
-        String[] parts = path.split("/");
-        Deque<String> deque = new LinkedList<>();
-
-        for (String part : parts) {
-            if (part.equals("") || part.equals(".")) {
-                continue;
-            } else if (part.equals("..")) {
-                if (!deque.isEmpty()) {
-                    deque.pollLast();
-                }
-            } else {
-                deque.offerLast(part);
+        String[] str=path.split("/");
+        Stack<String> st=new Stack<>();
+        String result="";
+        for(int i=0;i<str.length;i++){
+            if(str[i].equals(".")|| str[i].equals(""))continue;
+            if(str[i].equals("..")){
+                if(!st.isEmpty())st.pop();
             }
+            else st.push(str[i]);
         }
-        StringBuilder result = new StringBuilder();
-        for (String dir : deque) {
-            result.append("/").append(dir);
+        while(!st.isEmpty()){
+            result="/"+st.peek()+result;
+            st.pop();
         }
-        return result.length() > 0 ? result.toString() : "/";
+         if (result.isEmpty()){
+            result = "/";
+        }
+        return result;
     }
 }
+// class Solution {
+//     public String simplifyPath(String path) {
+//         String[] parts = path.split("/");
+//         Deque<String> deque = new LinkedList<>();
+
+//         for (String part : parts) {
+//             if (part.equals("") || part.equals(".")) {
+//                 continue;
+//             } else if (part.equals("..")) {
+//                 if (!deque.isEmpty()) {
+//                     deque.pollLast();
+//                 }
+//             } else {
+//                 deque.offerLast(part);
+//             }
+//         }
+//         StringBuilder result = new StringBuilder();
+//         for (String dir : deque) {
+//             result.append("/").append(dir);
+//         }
+//         return result.length() > 0 ? result.toString() : "/";
+//     }
+// }
