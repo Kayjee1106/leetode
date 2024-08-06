27d294932584b1d08609975df197534e45bced14
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-06 12:56:28 +0530

    Time: 4 ms (75.11%), Space: 44.5 MB (56.38%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int inc=0;
+        int exc=0;
+        int[] count=new int[10001];
+        for(int val:nums)count[val]++;
+        for(int i=0;i<10001;i++){
+            int ne=Math.max(inc,exc);
+            int ni=exc + count[i]*i;
+            
+            inc=ni;
+            exc=ne;
+        }
+        return Math.max(inc,exc);
+    }
+}
