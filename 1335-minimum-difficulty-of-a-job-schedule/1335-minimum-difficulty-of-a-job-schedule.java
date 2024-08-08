7c23cc2dfc3fd6ef53d973cb0d92264f0cf42d91
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-09 02:55:20 +0530

    Time: 178 ms (5.10%), Space: 44.8 MB (12.10%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -1,32 +1,29 @@
 public class Solution {
+    int[][]t;
     public int minDifficulty(int[] jobDifficulty, int d) {
-        int n = jobDifficulty.length;
-
-        if (n < d)
+        int n=jobDifficulty.length;
+        if(n<d){
             return -1;
-
-        int[][] t = new int[n + 1][d + 1];
-
-        // Base case: if there is only one day - Do all jobs on that day
-        for (int i = 0; i < n; i++) {
-            t[i][1] = Arrays.stream(Arrays.copyOfRange(jobDifficulty, i, n)).max().orElse(Integer.MIN_VALUE);
         }
-
-        // Build the DP table bottom-up
-        for (int days = 2; days <= d; days++) {
-            for (int i = 0; i <= n - days; i++) {
-                int maxDifficulty = Integer.MIN_VALUE;
-                int result = Integer.MAX_VALUE;
-
-                for (int j = i; j <= n - days; j++) {
-                    maxDifficulty = Math.max(maxDifficulty, jobDifficulty[j]);
-                    result = Math.min(result, maxDifficulty + t[j + 1][days - 1]);
-                }
-
-                t[i][days] = result;
-            }
+         t =new int[n][d + 1];
+        for (int[] row : t){
+            Arrays.fill(row,-1);
         }
-
-        return t[0][d];
+        return solve(jobDifficulty, n, 0, d);
+    }
+    private int solve(int[] jobDifficulty,int n,int idx,int d){
+        if(d==1){
+            return Arrays.stream(Arrays.copyOfRange(jobDifficulty, idx, n)).max().orElse(Integer.MIN_VALUE);
+        }
+        if(t[idx][d]!=-1){
+            return t[idx][d];
+        }
+        int max=Integer.MIN_VALUE;
+        int result=Integer.MAX_VALUE;
+        for(int i=idx;i<=n-d;i++){
+            max=Math.max(max,jobDifficulty[i]);
+            result=Math.min(result,max+solve(jobDifficulty,n,i+1,d-1));
+        }
+        return t[idx][d]=result;
     }
 }
