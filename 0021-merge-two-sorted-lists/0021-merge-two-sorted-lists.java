0ebf6de6f769b908ee82216a3c241d265223b1dc
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-14 16:25:04 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (42.10%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -12,33 +12,41 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
        if(list1==null)return list2;
        if(list2==null)return list1;
-       ListNode head=null,tail=null;
-       if(list1.val<list2.val){
-           head=tail=list1;
-           list1=list1.next;
-       }
+       // ListNode head=null,tail=null;
+       // if(list1.val<list2.val){
+       //     head=tail=list1;
+       //     list1=list1.next;
+       // }
+       //  else{
+       //      head=tail=list2;
+       //      list2=list2.next;
+       //  }
+       //  while(list1!=null && list2!=null){
+       //      if(list1.val<list2.val){
+       //          tail.next=list1;
+       //          tail=list1;
+       //          list1=list1.next;
+       //      }
+       //      else{
+       //          tail.next=list2;
+       //          tail=list2;
+       //          list2=list2.next;
+       //      }
+       //  }
+       //  if(list1==null){
+       //      tail.next=list2;
+       //  }
+       //  else{
+       //      tail.next=list1;
+       //  }
+       //  return head;
+        if(list1.val<=list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
+        }
         else{
-            head=tail=list2;
-            list2=list2.next;
+            list2.next=mergeTwoLists(list1,list2.next);
+            return list2;
         }
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                tail.next=list1;
-                tail=list1;
-                list1=list1.next;
-            }
-            else{
-                tail.next=list2;
-                tail=list2;
-                list2=list2.next;
-            }
-        }
-        if(list1==null){
-            tail.next=list2;
-        }
-        else{
-            tail.next=list1;
-        }
-        return head;
     }
 }
