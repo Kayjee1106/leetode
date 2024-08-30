a369d31cf0a26c51cd8989115b882bd3891d7937
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-30 17:59:11 +0530

    Time: 20 ms (5.31%), Space: 46 MB (10.76%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int height(TreeNode root){
+        if (root == null)
+            return 0;
+        else {
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+            if (lheight > rheight)
+                return (lheight + 1);
+            else
+                return (rheight + 1);
+        }
+    }
+
+    public double solve(TreeNode root, int level) {
+        if (root == null) return 0;
+
+        if (level == 1) {
+            return root.val;
+        } else if (level > 1) {
+            double leftSum = solve(root.left, level - 1);
+            double rightSum = solve(root.right, level - 1);
+            return leftSum + rightSum;
+        }
+        return 0;
+    }
+
+    public int countNodesAtLevel(TreeNode root, int level) {
+        if (root == null) return 0;
+
+        if (level == 1) {
+            return 1;
+        } else if (level > 1) {
+            int leftCount = countNodesAtLevel(root.left, level - 1);
+            int rightCount = countNodesAtLevel(root.right, level - 1);
+            return leftCount + rightCount;
+        }
+        return 0;
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        int h = height(root);
+        for (int i = 1; i <= h; i++) {
+            double sum = solve(root, i);
+            int count = countNodesAtLevel(root, i);
+            ans.add(sum / count);
+        }
+        return ans;
+    }
+}
