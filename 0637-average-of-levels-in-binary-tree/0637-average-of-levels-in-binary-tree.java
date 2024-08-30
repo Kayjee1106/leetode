344f5512b52529e297902a1a9377d21066c2da58
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-08-30 18:02:37 +0530

    Time: 20 ms (5.31%), Space: 46.1 MB (7.08%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -18,48 +18,41 @@ class Solution {
         if (root == null)
             return 0;
         else {
-            int lheight = height(root.left);
-            int rheight = height(root.right);
+            int lheight=height(root.left);
+            int rheight=height(root.right);
             if (lheight > rheight)
                 return (lheight + 1);
             else
                 return (rheight + 1);
         }
     }
-
-    public double solve(TreeNode root, int level) {
-        if (root == null) return 0;
-
-        if (level == 1) {
-            return root.val;
-        } else if (level > 1) {
-            double leftSum = solve(root.left, level - 1);
-            double rightSum = solve(root.right, level - 1);
-            return leftSum + rightSum;
+    public double solve(TreeNode root,int level){
+        if(root==null)return 0;
+        if(level==1)return root.val;
+        else if(level>1){
+            double left=solve(root.left,level-1);
+            double right=solve(root.right,level-1);
+            return left+right;
         }
         return 0;
     }
-
-    public int countNodesAtLevel(TreeNode root, int level) {
-        if (root == null) return 0;
-
-        if (level == 1) {
-            return 1;
-        } else if (level > 1) {
-            int leftCount = countNodesAtLevel(root.left, level - 1);
-            int rightCount = countNodesAtLevel(root.right, level - 1);
-            return leftCount + rightCount;
+    public int countNodesAtLevel(TreeNode root,int level){
+        if(root==null)return 0;
+        if(level==1)return 1;
+        else if(level>1){
+            int left=countNodesAtLevel(root.left,level-1);
+            int right=countNodesAtLevel(root.right,level-1);
+            return left+right;
         }
         return 0;
     }
-
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> ans = new ArrayList<>();
-        int h = height(root);
-        for (int i = 1; i <= h; i++) {
-            double sum = solve(root, i);
-            int count = countNodesAtLevel(root, i);
-            ans.add(sum / count);
+        List<Double> ans=new ArrayList<>();
+        int h=height(root);
+        for(int i=1;i<=h;i++){
+            double sum=solve(root, i);
+            int count=countNodesAtLevel(root,i);
+            ans.add(sum/(double)count);
         }
         return ans;
     }
