9eb43ba61374ff8d6d9d48a63f3daf698e3ddc11
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-09-15 17:34:22 +0530

    Time: 3 ms (61.34%), Space: 53.2 MB (21.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,21 +1,54 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int a=nums.length/2;
-        Map<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(mp.containsKey(nums[i])){
-               int x=mp.get(nums[i]);
-                x++;
-                mp.put(nums[i],x);
+    private int findCandidate(int a[]){
+        int x=0;
+        int size=a.length;
+        int count=1;
+        for(int i=1;i<size;i++){
+            if(a[x]==a[i]){
+                count++;
             }
-            else{
-                mp.put(nums[i],1);
+            else count--;
+            if(count==0){
+                x=i;
+                count=1;
             }
         }
-        for(Map.Entry<Integer,Integer> s: mp.entrySet()){
-            if(s.getValue()>a){
-                return s.getKey();
+        return a[x];
+    }
+    boolean isMajority(int[] arr,int cand){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==cand){
+                count++;
             }
+        }
+        if(count>arr.length/2){
+            return true;
+        }
+        return false;
+    }
+    public int majorityElement(int[] nums) {
+        // int a=nums.length/2;
+        // Map<Integer,Integer> mp=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(mp.containsKey(nums[i])){
+        //        int x=mp.get(nums[i]);
+        //         x++;
+        //         mp.put(nums[i],x);
+        //     }
+        //     else{
+        //         mp.put(nums[i],1);
+        //     }
+        // }
+        // for(Map.Entry<Integer,Integer> s: mp.entrySet()){
+        //     if(s.getValue()>a){
+        //         return s.getKey();
+        //     }
+        // }
+        // return 0;
+        int cand=findCandidate(nums);
+        if(isMajority(nums,cand)){
+            return cand;
         }
         return 0;
     }
