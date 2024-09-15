2348668444840985c158d157d4fd92035d332c8e
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-09-16 03:08:06 +0530

    Time: 4 ms (56.33%), Space: 42.6 MB (95.90%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,15 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i=0;
-        s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int j=s.length()-1;
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            // Move the left pointer forward if it's not an alphanumeric character
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+
+            // Move the right pointer backward if it's not an alphanumeric character
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            // Compare the characters after converting both to lowercase
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                 return false;
             }
+
             i++;
             j--;
         }
+
         return true;
-     }
+    }
 }
