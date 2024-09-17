55b04e4f5af5395027a677e45b8181f351774425
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-09-18 03:02:08 +0530

    Time: 6 ms (11.91%), Space: 42.1 MB (54.18%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.java
@@ -1,17 +1,25 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        String str=s1+" "+s2;
-        String[] strings=str.split(" ");
-        List<String> ans=new ArrayList<>();
-        Map<String,Integer> map=new HashMap<>();
-        for(int i=0;i<strings.length;i++){
-            map.put(strings[i],map.getOrDefault(strings[i],0)+1);
+        // Concatenate the two sentences with a space
+        String[] words = (s1 + " " + s2).split(" ");
+        
+        // Create a map to store the frequency of each word
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        
+        // Count the frequency of each word
+        for (String word : words) {
+            frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
         }
-        for(String key:map.keySet()){
-            if(map.get(key)==1){
-                ans.add(key);
+        
+        // Create a list for uncommon words (frequency = 1)
+        List<String> uncommonWords = new ArrayList<>();
+        for (String word : frequencyMap.keySet()) {
+            if (frequencyMap.get(word) == 1) {
+                uncommonWords.add(word);
             }
         }
-        return ans.toArray(new String[0]);
+        
+        // Return the list as a String array
+        return uncommonWords.toArray(new String[0]);
     }
 }
