e25b57da2f822e7e795ea0fc6b20b89c384c00b9
Kayjee1106 <117725512+Kayjee1106@users.noreply.github.com>
2024-10-02 13:17:19 +0530

    Time: 45 ms (44.64%), Space: 44.5 MB (30.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,4 +1,6 @@
 class Solution {
+    int[] t;
+    int n;
     public int lengthOfLIS(int[] nums) {
         int t[]=new int[nums.length];
         int max=0;
@@ -17,5 +19,23 @@ class Solution {
             }
         }
         return t[max]+1;
-    }
+    //     t=new int[2501];
+    //     Arrays.fill(t,-1);
+    //     n=nums.length;
+    //     return solve(nums,0,-1);
+ }
+    // public int solve(int []nums,int i,int p){
+    //     if(i>=n){
+    //         return 0;
+    //     }
+    //     if(t[i]!=-1){
+    //         return t[i];
+    //     } 
+    //     int take=0;
+    //     if(nums[i]>nums[p]){
+    //         take=1+solve(nums,i+1,i);
+    //     }
+    //     int leave=solve(nums,i+1,p);
+    //     return t[i]=Math.max(take,leave);
+    // }
 }
